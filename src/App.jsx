3f08a032ecd82fd2f4a137d3ff5b136d68f8dd71
argieuwu <argiepardillo009@gmail.com>
2025-06-14 20:06:08 +0800

    enhancing ui design

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
-import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
-import Overview from './pages/Overview/Overview';
-import Map from './pages/Map/Map';
+import Header from './components/Header/Header.jsx';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Overview from './pages/Overview/Overview.jsx';
+import Map from './pages/Map/Map.jsx';
 
 function App() {
   return (
-    <div className={styles.app}>
-      <Header />
-      <Sidebar />
-      <Overview />
-      <Map />
-    </div>
+    <BrowserRouter>
+      <div className={styles.app}>
+        <Header />
+        <div className={styles.mainContainer}>
+          <Sidebar />
+          <div className={styles.contentWrapper}>
+            <div className={styles.pageContent}>
+              <Routes>
+                <Route path="/overview" element={<Overview />} />
+                <Route path="/map" element={<Map />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
