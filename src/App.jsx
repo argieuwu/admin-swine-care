b106a9e7750b7595038505e0f19e9d6463b2c97b
argieuwu <argiepardillo009@gmail.com>
2025-06-15 21:58:54 +0800

    fixing navigation issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Navigate } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
 import Header from './components/Header/Header.jsx';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
 import Overview from './pages/Overview/Overview.jsx';
 import Map from './pages/Map/Map.jsx';
+import Login from './pages/Login/Login.jsx';
+import Register from './pages/Register/Register.jsx';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className={styles.app}>
-        <Header />
-        <div className={styles.mainContainer}>
-          <Sidebar />
-          <div className={styles.contentWrapper}>
-            <div className={styles.pageContent}>
-              <Routes>
-                <Route path="/overview" element={<Overview />} />
-                <Route path="/map" element={<Map />} />
-              </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="*"
+          element={
+            <div className={styles.app}>
+              <Header />
+              <div className={styles.mainContainer}>
+                <Sidebar />
+                <div className={styles.contentWrapper}>
+                  <div className={styles.pageContent}>
+                    <Routes>
+                      <Route path="/overview" element={<Overview />} />
+                      <Route path="/map" element={<Map />} />
+                    </Routes>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
