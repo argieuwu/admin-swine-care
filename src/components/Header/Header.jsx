784ad375acc66706e9145527590524dd4f157bd8
argieuwu <argiepardillo009@gmail.com>
2025-06-16 10:06:30 +0800

    enhancing header design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
     <header className={styles.headerContainer}>
       <div className={styles.navContainer}>
         <div className={styles.logo}>
-          <Link to="/">
-            <img src="/src/assets/SwineCareFinal.png" alt="Pig Icon" className={styles.logoIcon} />
+          <Link to="/overview">
+            <img src="/src/assets/SwineCareFinal.png" alt="ASF Tracker Icon" className={styles.logoIcon} />
             <span className={styles.logoText}>
               ASF <span className={styles.highlight}>Tracker</span>
             </span>
@@ -17,11 +17,6 @@ const Header = () => {
         <div className={styles.userProfile}>
           <div className={styles.userAvatar}>A</div>
           <span className={styles.userName}>Admin</span>
-          <button className={styles.dropdownToggle}>▼</button>
-          <div className={styles.dropdownMenu}>
-            <a href="/profile">Profile</a>
-            <a href="/logout">Logout</a>
-          </div>
         </div>
       </div>
     </header>
